58513b990d28435613b1380b780ee566d9557afc
Samundra111 <db3002448@gmail.com>
2026-04-17 12:33:44 +0545

    Deleted unnecessary files

--- ho.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-file=load_dotenv()
-
-print(".env file was found",load_dotenv)
-print(f"base url is {os.getenv('BASE_URL')}")
-print(f"base name is {os.getenv('API_KEY')}")
